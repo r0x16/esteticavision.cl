451ba8b9f709b1597f72fa60acd703bbad2d3010
Ribon Kelne Benath <ribonkb@esteticavision.local>
2017-11-10 20:06:32 -0300

    Added compatibility with older versions of MySQL and MariaDB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Esto soluciona un problema que existe con la actual versión de MariaDB
+        Schema::defaultStringLength(191);
     }
 
     /**
