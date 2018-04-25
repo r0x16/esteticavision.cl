b73197b0ea2e9321a5a4a3a0ef10f1139d054234
Ribon Kelne Benath <ribonkb@esteticavision.local>
2018-04-25 13:42:45 -0300

    Client Data corrected
    Se corrigen los datos de contacto del cliente.

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -12,7 +12,7 @@
             <div class="col-md-4">
                 <h4>Contactanos</h4>
                 <p>
-                    Antonio Varas 91 Oficina 208, Providencia.
+                    Doctor Manuel Barros Borgoño 71 Oficina 1105, Providencia.
                 </p>
                 <p>
                     (+569) 9256 2828
@@ -22,13 +22,13 @@
                 </p>
                 <p>
                     Horario de Atención:
-                    Lunes a Viernes de 08:00 a 16:00 hrs.
+                    Lunes a Viernes de 08:00 a 18:00 hrs.
                 </p>
             </div>
             <div class="col-md-4"></div>
             <div class="col-md-4 text-right">
                 <div class="map_footer">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.7882582739685!2d-70.61980168501724!3d-33.428764303785215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf633bfdaca1%3A0x82ba3b717465a36e!2sAntonio+Varas+91%2C+Providencia%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1sen!2scl!4v1522508123827" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.7565486454087!2d-70.62126898518387!3d-33.429590880779884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf7cd41a667b%3A0x927eec4909708101!2sDr+Manuel+Barros+Borgo%C3%B1o+71%2C+Providencia%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses-419!2scl!4v1524674382433" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 <p class="follow_footer">
                     Siguenos:
